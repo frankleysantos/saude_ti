d96e9e4174618f9aa73227a40297e7e7a88ef4d5
frankleysantos <frankleysilvasantos@gmail.com>
2022-10-05 15:12:36 -0300

    swagger procedimentos

--- app/Http/Controllers/Api/ProcedimentoController.php
+++ app/Http/Controllers/Api/ProcedimentoController.php
@@ -15,24 +15,103 @@ class ProcedimentoController extends Controller
         $this->procedimento = $service;
     }
 
+    /**
+     * @OA\Post(
+     *   tags={"Procedimento"},
+     *   security={{"bearer_token":{}}},
+     *   path="/procedimento/store",
+     *   summary="Procedimento store",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(
+     *       type="object",
+     *       required={"proc_nome", "proc_valor"},
+     *       @OA\Property(property="proc_nome", type="string"),
+     *       @OA\Property(property="proc_valor", type="float")
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=201,
+     *     description="OK",
+     *   ),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     */
     public function store(Request $procedimentoRequest) 
     {
         $procedimento = $this->procedimento->store($procedimentoRequest); 
         return response()->json($procedimento);
     }
 
+    /**
+     * @OA\Put(
+     *   tags={"Procedimento"},
+     *   security={{"bearer_token":{}}},
+     *   path="/procedimento/update/{id}",
+     *   summary="Procedimento update",
+     *   @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      required=true,
+     *   ),
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(property="proc_nome", type="string"),
+     *       @OA\Property(property="proc_valor", type="float")
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *   ),
+     *   @OA\Response(response=404, description="Not Found"),
+     *   @OA\Response(response=422, description="Unprocessable Entity")
+     * )
+     */
     public function update($codigo, Request $procedimentoRequest) 
     {
         $procedimento = $this->procedimento->update((int) $codigo, $procedimentoRequest);      
         return response()->json($procedimento);
     }
 
+    /**
+     * @OA\Delete(
+     *   tags={"Procedimento"},
+     *   path="/procedimento/delete/{id}",
+     *   security={{"bearer_token":{}}},
+     *   summary="Procedimento delete",
+     *   @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      required=true,
+     *   ),
+     *   @OA\Response(response=200, description="OK"),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     */
     public function delete($codigo)
     {
         $message = $this->procedimento->delete($codigo);    
         return response()->json($message);
     }
 
+    /**
+     * @OA\Get(
+     *   tags={"Procedimento"},
+     *   security={{"bearer_token":{}}},
+     *   path="/procedimento/show",
+     *   summary="Procedimento show",
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *   ),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     */
     public function show()
     {
         $response = $this->procedimento->getAll();
--- routes/groupApi/ProcedimentoApi.php
+++ routes/groupApi/ProcedimentoApi.php
@@ -7,7 +7,7 @@ Route::group([
     'prefix' => 'auth/procedimento'
 ], function ($router) {
     Route::post('/store', [ProcedimentoController::class, 'store']);
-    Route::post('/update/{id}', [ProcedimentoController::class, 'update']);
+    Route::put('/update/{id}', [ProcedimentoController::class, 'update']);
     Route::delete('/delete/{id}', [ProcedimentoController::class, 'delete']); 
     Route::get('/show', [ProcedimentoController::class, 'show'])->name('api.client.show'); 
 });
